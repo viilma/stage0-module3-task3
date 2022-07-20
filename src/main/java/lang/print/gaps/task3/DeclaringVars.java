aa65a52a0338d8703ed2c31e67af8bcbd1ca4705
Vilma Zaveckienė <v.zaveckiene@gmail.com>
2022-07-20 20:09:19 +0300

    One-line variable declaration and initialization fixed

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,9 +2,10 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        int c = 10;
+
+        int a, b, c;
+
+        a = b = c = 10;
 
         System.out.println(c);
     }
