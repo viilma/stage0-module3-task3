2e25f132c19928b4bc3c1bc5eddda40434fe5a6a
Vilma Zaveckienė <v.zaveckiene@gmail.com>
2022-07-20 19:41:01 +0300

    One-line variable declaration and initialization done

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,6 +2,10 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int a = 10;
+        int b = 10;
+        int c = 10;
+
+        System.out.println(c);
     }
 }
